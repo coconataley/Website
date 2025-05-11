d3a60e6a1610bfa4f2205412af1beb3a15784bd4
coconataley <nathaliebiana@gmail.com>
2025-05-11 17:32:59 +0800

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,30 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'group1.gena@gmail.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+
+$email_body = "User Name: $name.\n".
+				"User Email: $visitor_email.\n".
+				"Subject: $subject.\n".
+				"User Message: $message.\n".
+
+$to = 'nathaliebiana@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers = "Reply-to: $visitor_from \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+					
+				
+mail($to,$email_subject,$email_body,$headers)
+
+header("Location: contact.html");
+?>
